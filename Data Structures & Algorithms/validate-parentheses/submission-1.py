41066859e552782f3f40363ee243c489410de111
Shashank <67432940+shashankpal1909@users.noreply.github.com>
2026-07-29 22:13:17 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = deque()
+        bracket_map = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in ["[", "(", "{"]:
+                stack.append(char)
+            else:
+                if len(stack) == 0 or stack[-1] != bracket_map.get(char):
+                    return False
+                stack.pop()
+            
+            print(stack)
+
+        return len(stack) == 0
